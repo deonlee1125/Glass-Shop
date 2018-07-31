496deb190eab54751a59b1db39f71ad1d0beb7e4
Deon Lee <jamie2deon@aol.com>
2018-07-31 14:05:16 -0500

    Added "Add Art" form, which adds only art type (vase, bowl, etc.), not full description of artwork. Also, add art displaying on landing page. Should be login form.

--- src/main/java/com/example/glassshop/controllers/ArtController.java
+++ src/main/java/com/example/glassshop/controllers/ArtController.java
@@ -6,37 +6,49 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("art")
 public class ArtController {
 
+    static ArrayList<String> arts = new ArrayList<>();
+
     @RequestMapping(value = "")
     public String index(Model model) {
+
+        model.addAttribute("arts", arts);
         model.addAttribute("title", "Welcome to ARTracker!");
         return "art/index";
     }
-}
 
-     /*@RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model) {
 
-        //model.addAttribute("cheeses", cheeseDao.findAll());
-        model.addAttribute("title", "Welcome to ARTracker!");
+    /*@RequestMapping(value = "", method = RequestMethod.GET)
+   public String index(Model model) {
 
-        return "art/index";
-    }
-}
-   @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCheeseForm(Model model) {
-        model.addAttribute("title", "Add Cheese");
-        model.addAttribute(new Cheese());
-        model.addAttribute("categories", categoryDao.findAll());
-        return "cheese/add";
+       //model.addAttribute("cheeses", cheeseDao.findAll());
+       model.addAttribute("title", "Welcome to ARTracker!");
+
+       return "art/index";
+   }
+}*/
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddArtForm(Model model) {
+        model.addAttribute("title", "Add Art");
+        //model.addAttribute(new Art());
+        //model.addAttribute("categories", categoryDao.findAll());
+        return "art/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese,
+    public String processAddArtForm(@RequestParam String artType) {
+        arts.add(artType);
+        return "redirect:";
+    }
+}
+
+
+    /*public String processAddArtForm(@ModelAttribute @Valid Cheese newCheese,
                                        Errors errors, @RequestParam int categoryId, Model model) {
 
         if (errors.hasErrors()) {
