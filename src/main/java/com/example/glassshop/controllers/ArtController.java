60f34ca9e269902f5c1bccb385369e06453c2598
Deon Lee <jamie2deon@aol.com>
2018-07-31 10:42:52 -0500

    Landing page title with view passing from model

--- src/main/java/com/example/glassshop/controllers/ArtController.java
+++ src/main/java/com/example/glassshop/controllers/ArtController.java
@@ -1,15 +1,52 @@
 package com.example.glassshop.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("art")
 public class ArtController {
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping(value = "")
     public String index(Model model) {
-        //model.addAttribute("categories", categoryDao.findAll());
-        model.addAttribute("title", "Yay!");
-        return "index";
+        model.addAttribute("title", "Welcome to ARTracker!");
+        return "art/index";
     }
 }
+
+     /*@RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
+
+        //model.addAttribute("cheeses", cheeseDao.findAll());
+        model.addAttribute("title", "Welcome to ARTracker!");
+
+        return "art/index";
+    }
+}
+   @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new Cheese());
+        model.addAttribute("categories", categoryDao.findAll());
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese,
+                                       Errors errors, @RequestParam int categoryId, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Cheese");
+            model.addAttribute( "categories", categoryDao.findAll());
+            return "cheese/add";
+        }
+
+        Category cat = categoryDao.findOne(categoryId);
+        newCheese.setCategory(cat);
+        cheeseDao.save(newCheese);
+        return "redirect:";
+    }*/
