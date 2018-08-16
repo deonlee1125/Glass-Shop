9fca9d358841272c612c172873871ce0ada4913c
Deon Lee <jamie2deon@aol.com>
2018-08-16 15:14:46 -0500

    Art is displaying and beginning of search controller working. Added user login and registration page but  not functioning.

--- src/main/java/com/example/glassshop/controllers/HomeController.java
+++ src/main/java/com/example/glassshop/controllers/HomeController.java
@@ -2,7 +2,12 @@ package com.example.glassshop.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("")
@@ -13,4 +18,17 @@ public class HomeController {
         model.addAttribute("title", "Welcome to ARTracker!");
         return "home/index";
     }
+/*
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String displayLoginForm (Model model) {
+        model.addAttribute("title", "Log in to ARTracker");
+        return "home/login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String processLoginForm (Model model, @ModelAttribute, @Valid User newUser, Errors errors) {
+        if (errors.hasErrors())
+            model.addAttribute("title", "Log in to ARTracker");
+        return "home/login";
+    } */
 }
