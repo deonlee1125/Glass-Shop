65b5e3d61195d5a030cd61c6e32578fac13c7829
Deon Lee <jamie2deon@aol.com>
2018-08-20 10:43:16 -0500

    Can search by artist name, description keyword, or tracking number. However, need to work out how to separate each search. Currently searches on all.

--- src/main/java/com/example/glassshop/controllers/SearchController.java
+++ src/main/java/com/example/glassshop/controllers/SearchController.java
@@ -2,22 +2,19 @@ package com.example.glassshop.controllers;
 
 import com.example.glassshop.models.Art;
 import com.example.glassshop.models.ArtistName;
-import com.example.glassshop.models.SearchFieldType;
 import com.example.glassshop.models.data.ArtDao;
 import com.example.glassshop.models.forms.SearchForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.hibernate.internal.util.collections.CollectionHelper.arrayList;
-
 
 @Controller
 @RequestMapping("search")
@@ -29,35 +26,39 @@ public class SearchController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String search(Model model) {
-        model.addAttribute("art", artDao.findAll());
+        model.addAttribute("artistNames", ArtistName.values());
         model.addAttribute("title", "Search Art");
         model.addAttribute(new SearchForm());
-        model.addAttribute(new Art());
-        model.addAttribute("artistNames", ArtistName.values());
         return "art/search";
     }
 
     @RequestMapping(value = "results", method = RequestMethod.POST)
     public String search(Model model,
-                         @ModelAttribute SearchForm searchForm) {
-        model.addAttribute(searchForm);
-        if (searchForm.getSearchField().equals("Description")) {
-            List art = artDao.findByDescription(searchForm.getKeyword());
-            model.addAttribute("art", art);
+                         @ModelAttribute @Valid SearchForm searchForm, Errors errors) {
 
+        Iterable<Art> arts = artDao.findAll();
+        List<Art> artSearch = new ArrayList<>();
+
+        for(Art art : arts) {
+
+            //if (searchForm.getSearchField().equals(artistName)) {
+            if (searchForm.getArtistName().equals(art.getArtistName())) {
+                artSearch.add(art);
+            }
+
+            if (art.getDescription().contains(searchForm.getDescription())) {
+                artSearch.add(art);
+            }
+
+            if (searchForm.getTrackingNumber().equals(art.getTrackingNumber())) {
+                artSearch.add(art);
+            }
         }
+        model.addAttribute("title", "Search Results");
+        model.addAttribute("artistNames", ArtistName.values());
+        model.addAttribute("artSearch", artSearch);
 
-        return "art/search";
+        return "art/search-results";
     }
 
 }
-
-/*
-        if (searchForm.getSearchField().equals(JobFieldType.ALL)) {
-                jobs = jobData.findByValue(searchForm.getKeyword());
-                } else {
-                jobs = jobData.findByColumnAndValue(searchForm.getSearchField(), searchForm.getKeyword());
-                }
-
-                model.addAttribute("jobs", jobs);
-                */
